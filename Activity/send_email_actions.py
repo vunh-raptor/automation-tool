2720701a0d309a46de9cd10431b16b6dbf98dfe0
Thong Nguyen Huy (VN) <Thong.NguyenH1@homecredit.vn>
2024-11-25 15:01:44 +0700

    adding BCC option and reformat code

--- Activity/send_email_actions.py
+++ Activity/send_email_actions.py
@@ -4,31 +4,38 @@ import streamlit.components.v1 as components
 import os
 
 
-def send_email_automation (receiver: str ,cc : str, emailSubject : str, bodyTemplate : str):
+def send_email_automation(
+    receiver: str, cc: str, bcc: str, emailSubject: str, bodyTemplate: str
+):
     """This funtion is will execute step by step sending an email using python
 
     Args:
-        receiver (str): receivers of the email 
+        receiver (str): receivers of the email
         cc (str): cc of the email
         emailSubject (str): subject of the email
         bodyTemplate (str): email content
     """
-    #intergrate with outlook
-    outlook = win32. Dispatch('outlook.application', pythoncom.CoInitialize())
-    #create an email
+    # intergrate with outlook
+    outlook = win32.Dispatch("outlook.application", pythoncom.CoInitialize())
+    # create an email
     email = outlook.CreateItem(0)
-    #email body
+    # email body
     email.To = receiver
     email.CC = cc
+    email.BCC = bcc
     email.Subject = emailSubject
     email.HTMLBody = bodyTemplate
-    #Get the path of the file on local device
+    # Get the path of the file on local device
     current_directory = os.path.dirname(__file__)
-    relative_path = os.path.join(current_directory, '..','Common', 'template', 'maintenace_images.jpg')
-    #This code is for sending by specific email
+    relative_path = os.path.join(
+        current_directory, "..", "Common", "template", "maintenace_images.jpg"
+    )
+    # This code is for sending by specific email
     # The e-mail needs to be part of your outlook account.
-    attachment = email.Attachments.Add(relative_path) 
-    attachment.PropertyAccessor.SetProperty("http://schemas.microsoft.com/mapi/proptag/0x3712001F", "image_id")
+    attachment = email.Attachments.Add(relative_path)
+    attachment.PropertyAccessor.SetProperty(
+        "http://schemas.microsoft.com/mapi/proptag/0x3712001F", "image_id"
+    )
     From = None
     for myEmailAddress in outlook.Session.Accounts:
         if "_IT_SD@homecredit.vn" in str(myEmailAddress):
@@ -38,7 +45,6 @@ def send_email_automation (receiver: str ,cc : str, emailSubject : str, bodyTemp
     if From != None:
         # This line basically calls the "mail.SendUsingAccount = specific account" outlook VBA command
         email._oleobj_.Invoke(*(64209, 0, 8, 0, From))
-    
+
     email.Display(False)
     email.Send()
-
--- pages/3_Send_Email_Automation.py
+++ pages/3_Send_Email_Automation.py
@@ -5,147 +5,212 @@ import streamlit.components.v1 as components
 from streamlit_quill import st_quill
 from streamlit_ace import st_ace
 
-from Activity.send_email_actions import(
-    send_email_automation
-)
-
+from Activity.send_email_actions import send_email_automation
 
 
 def main():
-#This function is to working with sending email task
+    # This function is to working with sending email task
 
-   #Title of the page
-   st.title("Email Automation Hub")
+    # Title of the page
+    st.title("Email Automation Hub")
 
-   #Option for chose email template
-   option = st.selectbox(
+    # Option for chose email template
+    option = st.selectbox(
         "Please chose the email template",
-        (   "External Maintenance Notification",
-            "Internal Maintenance Notification", 
+        (
+            "External Maintenance Notification",
+            "Internal Maintenance Notification",
             "Incident Notification",
-            ),
+        ),
         index=None,
-        )
-   st.divider()
-   #Send External Maintenance Notification email
-   if option == "External Maintenance Notification":
-        #Input fileds of external maintenance email
-        left, rigth = st.columns(2, vertical_alignment="bottom")     
+    )
+    st.divider()
+    # Send External Maintenance Notification email
+    if option == "External Maintenance Notification":
+        # Input fileds of external maintenance email
+        left, rigth = st.columns(2, vertical_alignment="bottom")
         start_date = left.date_input(label="Ngày bắt đầu")
-        start_time = rigth.time_input(label= "Thời gian bắt đầu")
+        start_time = rigth.time_input(label="Thời gian bắt đầu")
         end_date = left.date_input(label="Ngày kết thúc")
-        end_time = rigth.time_input(label= "Thời gian kết thúc")
+        end_time = rigth.time_input(label="Thời gian kết thúc")
         start_datetime = datetime.datetime.combine(start_date, start_time)
         end_datetime = datetime.datetime.combine(end_date, end_time)
-        #Format date and time for English version
+        # Format date and time for English version
         start_datetime_eng = start_datetime.strftime("%B %d, %Y, %X")
         end_datetime_eng = start_datetime.strftime("%B %d, %Y, %X")
-                  
-        #Array contain value fill in email
-        date_start_and_end = {'start': start_datetime, 'end': end_datetime, 'start_eng':start_datetime_eng, 'end_eng':end_datetime_eng }
-        
+
+        # Array contain value fill in email
+        date_start_and_end = {
+            "start": start_datetime,
+            "end": end_datetime,
+            "start_eng": start_datetime_eng,
+            "end_eng": end_datetime_eng,
+        }
+
         st.divider()
 
-        #Receiver and cc fields
+        # Receiver and cc fields
         to = st.text_input("To")
         cc = st.text_input("CC")
+        bcc = st.text_input("BCC")
 
-        #Define email html template
-        HTMLFile = open(r"Common\template\external_mail_maintenance_template.html", "r", encoding="utf8")
-        #Load html template
+        # Define email html template
+        HTMLFile = open(
+            r"Common\template\external_mail_maintenance_template.html",
+            "r",
+            encoding="utf8",
+        )
+        # Load html template
         external_maintenance_notification_html = HTMLFile.read()
 
+        # Fill the input value to the email template and show it in a editor section
+        external_mail_maintenance_content = st_ace(
+            value=external_maintenance_notification_html.format(**date_start_and_end),
+            auto_update=True,
+        )
 
-
-        #Fill the input value to the email template and show it in a editor section
-        external_mail_maintenance_content = st_ace(value = external_maintenance_notification_html.format(**date_start_and_end), auto_update= True)
-        
-        #Show the email content on UI
+        # Show the email content on UI
         st.markdown(external_mail_maintenance_content, unsafe_allow_html=True)
-        
-        left,middle, rigth = st.columns(3, vertical_alignment="bottom")
 
-        #Send the full email by click on the button
-        send_email_btn = middle.button("Send External Maintenance Notification email", type= "primary")
+        left, middle, rigth = st.columns(3, vertical_alignment="bottom")
+
+        # Send the full email by click on the button
+        send_email_btn = middle.button(
+            "Send External Maintenance Notification email", type="primary"
+        )
         if send_email_btn:
-            send_email_automation(to,cc,"External Maintenance Notification",external_mail_maintenance_content)
-   
-    #Send Internal Maintenance Notification email
-   if option == "Internal Maintenance Notification":
-        #Input fileds of internal maintenance email
-        subject = st.text_input("Subject of the email",max_chars=250)
+            send_email_automation(
+                receiver=to,
+                cc=cc,
+                bcc=bcc,
+                emailSubject="[MAINTENANCE NOTIFICATION] HCVN THÔNG BÁO BẢO TRÌ HỆ THỐNG ",
+                bodyTemplate=external_mail_maintenance_content,
+            )
+
+    # Send Internal Maintenance Notification email
+    if option == "Internal Maintenance Notification":
+        # Input fileds of internal maintenance email
+        subject = st.text_input("Subject of the email", max_chars=250)
         description = st.text_input("Email description")
         left, rigth = st.columns(2, vertical_alignment="bottom")
         start_date = left.date_input(label="Ngày bắt đầu")
-        start_time = rigth.time_input(label= "Thời gian bắt đầu")
+        start_time = rigth.time_input(label="Thời gian bắt đầu")
         end_date = left.date_input(label="Ngày kết thúc")
-        end_time = rigth.time_input(label= "Thời gian kết thúc")
+        end_time = rigth.time_input(label="Thời gian kết thúc")
         start_datetime = datetime.datetime.combine(start_date, start_time)
         end_datetime = datetime.datetime.combine(end_date, end_time)
-        #Format date and time for English version
+        
+        # Format date and time for English version
         start_datetime_eng = start_datetime.strftime("%B %d, %Y, %X")
         end_datetime_eng = end_datetime.strftime("%B %d, %Y, %X")
         impacted_service = st.text_input("Impacted service")
-        
+
         st.divider()
-        
-        #Receiver and cc fields
+
+        # Receiver and cc fields
         to = st.text_input("To")
         cc = st.text_input("CC")
+        bcc = st.text_input("BCC")
 
+        # Array contain value fill in email
+        filled_value = {
+            "description": description,
+            "start": start_datetime_eng,
+            "end": end_datetime_eng,
+            "impactedService": impacted_service,
+        }
 
-        #Array contain value fill in email
-        filled_value = { 'description': description, 'start':start_datetime_eng, 'end':end_datetime_eng, 'impactedService':impacted_service }
-        
-        #Define email html template
-        HTMLFile = open(r"Common\template\internal_mail_maintenance_template.html", "r", encoding="utf8")
-        #Load html template
+        # Define email html template
+        HTMLFile = open(
+            r"Common\template\internal_mail_maintenance_template.html",
+            "r",
+            encoding="utf8",
+        )
+        # Load html template
         internal_maintenance_notification_html = HTMLFile.read()
-        #Fill the input value to the email template and show it in editor section
-        internal_mail_maintenance_content = st_ace(value = internal_maintenance_notification_html.format(**filled_value), auto_update= True)
-        
-        #Show the email content on UI
+        # Fill the input value to the email template and show it in editor section
+        internal_mail_maintenance_content = st_ace(
+            value=internal_maintenance_notification_html.format(**filled_value),
+            auto_update=True,
+        )
+
+        # Show the email content on UI
         st.markdown(internal_mail_maintenance_content, unsafe_allow_html=True)
 
-        left,middle, rigth = st.columns(3, vertical_alignment="bottom")
+        left, middle, rigth = st.columns(3, vertical_alignment="bottom")
 
-        #Send the full email by click on the button
-        send_email_btn = middle.button("Send Internal Maintenance Notification email", type= "primary")
+        # Send the full email by click on the button
+        send_email_btn = middle.button(
+            "Send Internal Maintenance Notification email", type="primary"
+        )
         if send_email_btn:
-            send_email_automation(to, cc,"[MAINTENANCE NOTIFICATION] " + subject, internal_mail_maintenance_content)
+            send_email_automation(
+                receiver=to,
+                cc=cc,
+                bcc=bcc,
+                emailSubject="[MAINTENANCE NOTIFICATION] " + subject,
+                bodyTemplate=internal_mail_maintenance_content,
+            )
 
-    #Send Incident Notification email
-   if option == "Incident Notification":
-       #Input fileds of incident notification email
-       incident_ticket_number = st.text_input("Input number of ticket incident",value= "INCVN-", max_chars=10)
-       incident_description = st.text_input("Email Description")
-       bussiness_impacted = st.text_input("Business Impacted")
-       current_status = st.text_input("Current Status")
+    # Send Incident Notification email
+    if option == "Incident Notification":
+        # Input fileds of incident notification email
+        incident_ticket_number = st.text_input(
+            "Input number of ticket incident", value="INCVN-", max_chars=10
+        )
+        incident_description = st.text_input("Email Description")
+        bussiness_impacted = st.text_input("Business Impacted")
+        current_status = st.text_input("Current Status")
 
-       st.divider()
+        st.divider()
 
-       #Receiver and cc fields
-       to = st.text_input("To")
-       cc = st.text_input("CC")
+        # Receiver and cc fields
+        to = st.text_input("To")
+        cc = st.text_input("CC")
+        bcc = st.text_input("BCC")
 
-      
-       incident_email_filled_value = { 'ticketNumber': incident_ticket_number, 'description':incident_description, 'businessImpact':bussiness_impacted, 'currentStatus':current_status }
-        #Define email html template
-       HTMLFile = open(r"Common\template\incident_notification_template.html", "r", encoding="utf8")
-       #Load html template
-       incident_notification_template_html = HTMLFile.read()
-       #Fill the input value to the email template and show it in editor section
-       incident_notification_content = st_ace(value = incident_notification_template_html.format(**incident_email_filled_value), auto_update= True)
-       #Show the email content on UI
-       st.markdown(incident_notification_content, unsafe_allow_html=True)
+        incident_email_filled_value = {
+            "ticketNumber": incident_ticket_number,
+            "description": incident_description,
+            "businessImpact": bussiness_impacted,
+            "currentStatus": current_status,
+        }
+        # Define email html template
+        HTMLFile = open(
+            r"Common\template\incident_notification_template.html", "r", encoding="utf8"
+        )
+        # Load html template
+        incident_notification_template_html = HTMLFile.read()
+        # Fill the input value to the email template and show it in editor section
+        incident_notification_content = st_ace(
+            value=incident_notification_template_html.format(
+                **incident_email_filled_value
+            ),
+            auto_update=True,
+        )
+        # Show the email content on UI
+        st.markdown(incident_notification_content, unsafe_allow_html=True)
 
-       left,middle, rigth = st.columns(3, vertical_alignment="bottom")
+        left, middle, rigth = st.columns(3, vertical_alignment="bottom")
 
-       #Send the full email by click on the button
-       send_email_btn = middle.button("Send Incident Notification email", )
-       if send_email_btn:
-        send_email_automation(to,cc,"[INCIDENT NOTIFICATION]-" + "[" + incident_ticket_number + "-" + incident_description + "]" , incident_notification_content)       
+        # Send the full email by click on the button
+        send_email_btn = middle.button(
+            "Send Incident Notification email",
+        )
+        if send_email_btn:
+            send_email_automation(
+                receiver=to,
+                cc=cc,
+                bcc=bcc,
+                emailSubject="[INCIDENT NOTIFICATION]-"
+                + "["
+                + incident_ticket_number
+                + "-"
+                + incident_description
+                + "]",
+                bodyTemplate=incident_notification_content,
+            )
 
-       
+
 if __name__ == "__main__":
     main()
