f37e39e3f6b290c99a06ab55facdad7cd7308775
Thong Nguyen Huy (VN) <Thong.NguyenH1@homecredit.vn>
2024-11-25 11:06:01 +0700

    Fixing requirements.txt and changing the update_scripts.ps1 to reinstall the requirements packages

--- update_script.ps1
+++ update_script.ps1
@@ -7,9 +7,11 @@ The script performs the following steps:
 1. Activates a Python virtual environment located at .\venv\Scripts\.
 2. Fetches the latest changes from the remote git repository.
 3. Merges the fetched changes into the current branch.
+4. Installs the required Python packages listed in the requirements.txt file.
 
 #>
 
 .\venv\Scripts\Activate.ps1
 git fetch
 git pull
+pip install -r requirements.txt
